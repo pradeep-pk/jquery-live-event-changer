a44a979aa028db06a0b12ebcc227ef944f27caa3
Pradeepkumar <pradeep.pk@outlook.com>
2023-05-02 17:58:27 +0530

    Update jquery_live_event_changer.py

--- jquery_live_event_changer.py
+++ jquery_live_event_changer.py
@@ -1,35 +1,36 @@
 import os
 import re
-
-# Set the directory to scan
-directory = 'path/to/change/files'
+import argparse
 
 # Define the regex pattern to match .live() events
-#pattern = r'\$\(['\"](.+?)['\"]\)\.live\(\s*("[^"]*")\s*,\s*(\w+)\s*\);'
 pattern = r"\$\(['\"](.+?)['\"]\)\.live\(\s*['\"](.+?)['\"]\s*,\s*(.*)"
-#pattern = r"\$\(['\"](.+?)['\"]\)\.(?:live|bind)\(\s*['\"](.+?)['\"]\s*,\s*(\w+)\s*\)|\$\(['\"](.+?)['\"]\)\.on\(\s*['\"](.+?)['\"]\s*,\s*(\w+)\s*\)"
-
-#pattern = r'\$\(("[^"]*")\)\.live\(\s*("[^"]*")\s*,\s*(\w+)\s*\);'
-#pattern2 = r'\$\(("[^"]*")\)\.live\(\s*("[^"]*")\s*,\s*(\w+)\s*\(\)\s*\{'
-
-#pattern2 = r'\$\(("[^"]*")\)\.live\(\s*("[^"]*")\s*,\s*(\w+)\s*\(\)\{'
-
-# Define the replacement string with the selector parameter
 replacement = r'$(document).on("\2", "\1", \3'
-#replacement2 = r'$(document).on(\2, \1, \3{'
 
+def replace_live_events(directory):
+    # Recursively scan the directory for .aspx and .js files
+    for subdir, dirs, files in os.walk(directory):
+        print("Currently working on " + subdir + "...")
+        for file in files:
+            filepath = os.path.join(subdir, file)
+            if filepath.endswith('.aspx') or filepath.endswith('.js'):
+                # Open the file and read its contents
+                with open(filepath, 'r') as f:
+                    content = f.read()
+                    # Replace all instances of .live() with $(document).on() using the selector parameter
+                    new_content = re.sub(pattern, replacement, content)
+                    #new_content = re.sub(pattern, replacement2, content)
+                # Write the new contents back to the file
+                with open(filepath, 'w') as f:
+                    f.write(new_content)
+        print("Completed: " + subdir)
 
-# Recursively scan the directory for .aspx and .js files
-for subdir, dirs, files in os.walk(directory):
-    for file in files:
-        filepath = os.path.join(subdir, file)
-        if filepath.endswith('.aspx') or filepath.endswith('.js'):
-            # Open the file and read its contents
-            with open(filepath, 'r') as f:
-                content = f.read()
-                # Replace all instances of .live() with $(document).on() using the selector parameter
-                new_content = re.sub(pattern, replacement, content)
-                #new_content = re.sub(pattern, replacement2, content)
-            # Write the new contents back to the file
-            with open(filepath, 'w') as f:
-                f.write(new_content)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("arg", help="argument to be passed to the script")
+    args = parser.parse_args()
+    print("Replacing .live() events to $(document).on events...")
+    replace_live_events(args.arg)
+    print("Replaced successfully!")
+
+if __name__ == '__main__':
+    main();
